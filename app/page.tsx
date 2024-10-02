20a5cb1a91c239dc127c049ada19d08eaca2fa6a
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-10-02 18:53:46 +0530

    d

--- app/page.tsx
+++ app/page.tsx
@@ -17,14 +17,16 @@ export default function Home() {
         data-action="focus->s-popover#show" data-s-popover-placement="bottom-start"
     aria-expanded="false"/>
     <span data-view-component="true">
-  <clipboard-copy id="clipboard-button" aria-label="Copy" type="button" value="Text to copy" data-view-component="true" class="Button--secondary Button--medium Button">
-    <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-copy">
-      <path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path><path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path>
-    </svg>
-    <svg style="display: none;" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-check color-fg-success">
-      <path d="M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z"></path>
-    </svg>
-  </clipboard-copy>  
+      <clipboard-copy id="clipboard-button" aria-label="Copy" type="button" value="Text to copy"
+        data-view-component="true" class="Button--secondary Button--medium Button">
+        <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16"
+          data-view-component="true" class="octicon octicon-copy">
+          <path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path><path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path>
+        </svg>
+        <svg style={{display: 'none'}} aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-check color-fg-success">
+          <path d="M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z"></path>
+        </svg>
+      </clipboard-copy>
   <div aria-live="polite" aria-atomic="true" class="sr-only" data-clipboard-copy-feedback></div>
 </span><tool-tip id="tooltip-47ec3eb3-b354-45e8-87b5-43045b55d353" for="clipboard-button" popover="manual" data-direction="s" data-type="description" data-view-component="true" class="sr-only position-absolute">Copy some text</tool-tip>
 {/* <button data-component="IconButton" onMouseEnter={ () => {p = 1} }
