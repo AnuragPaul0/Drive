e3e1833e69e1999c0d12845bdd1a22c9cc2f3395
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-10-05 12:10:00 +0000

    CopyToClipboardButton

---
+++ assets/ui/packages/copy-to-clipboard/Button/index.js
@@ -0,0 +1,5 @@
+import CopyToClipboardButton from "../CopyToClipboardButton.tsx";
+// import { arrayMove, arrayRemove } from "./utils.js";
+export { CopyToClipboardButton
+    // , arrayMove, arrayRemove 
+};
