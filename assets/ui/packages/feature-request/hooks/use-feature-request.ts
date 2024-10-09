212d96e4b96d462698c8554cceec84689095db2a
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-10-09 14:15:01 +0000

    f

--- assets/ui/packages/feature-request/hooks/use-feature-request.ts
+++ assets/ui/packages/feature-request/hooks/use-feature-request.ts
@@ -1,7 +1,7 @@
-import {useState} from 'react'
+// import {useState} from 'react'
 // eslint-disable-next-line no-restricted-imports
-import {useToastContext} from '@github-ui/toast/ToastContext'
-import {createRequest, deleteRequest} from '../../services/api'
+// import {useToastContext} from '@github-ui/toast/ToastContext'
+// import {createRequest, deleteRequest} from '../../services/api'
 
 export interface FeatureRequestState {
   inProgress: boolean
@@ -19,36 +19,36 @@ export interface FeatureRequestInfo {
   dismissedAt?: string
 }
 
-export function useFeatureRequest(featureRequestInfo?: FeatureRequestInfo): FeatureRequestState {
-  const {
-    alreadyRequested = false,
-    dismissed = false,
-    dismissedAt = '',
-    featureName = '',
-    requestPath = '',
-  } = featureRequestInfo ?? {}
-  const [inProgress, setInProgress] = useState(false)
-  const [requested, setRequested] = useState(alreadyRequested)
-  const {addToast} = useToastContext()
+// export function useFeatureRequest(featureRequestInfo?: FeatureRequestInfo): FeatureRequestState {
+//   const {
+//     alreadyRequested = false,
+//     dismissed = false,
+//     dismissedAt = '',
+//     featureName = '',
+//     requestPath = '',
+//   } = featureRequestInfo ?? {}
+//   const [inProgress, setInProgress] = useState(false)
+//   const [requested, setRequested] = useState(alreadyRequested)
+//   const {addToast} = useToastContext()
 
-  const toggleFeatureRequest = async () => {
-    setInProgress(true)
-    const request = requested ? deleteRequest : createRequest
-    const success = await request(requestPath, featureName)
-    if (success) {
-      setRequested(!requested)
-    } else {
-      // eslint-disable-next-line @github-ui/dotcom-primer/toast-migration
-      addToast({type: 'error', message: 'Something went wrong. Please try again later.'})
-    }
-    setInProgress(false)
-  }
+//   const toggleFeatureRequest = async () => {
+//     setInProgress(true)
+//     const request = requested ? deleteRequest : createRequest
+//     const success = await request(requestPath, featureName)
+//     if (success) {
+//       setRequested(!requested)
+//     } else {
+//       // eslint-disable-next-line @github-ui/dotcom-primer/toast-migration
+//       addToast({type: 'error', message: 'Something went wrong. Please try again later.'})
+//     }
+//     setInProgress(false)
+//   }
 
-  return {
-    inProgress,
-    requested,
-    dismissed,
-    dismissedAt,
-    toggleFeatureRequest,
-  }
-}
+//   return {
+//     inProgress,
+//     requested,
+//     dismissed,
+//     dismissedAt,
+//     toggleFeatureRequest,
+//   }
+// }
