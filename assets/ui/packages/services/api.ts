ef6728293c3dff17e28a6d4c160db5571f12756b
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-10-09 14:12:18 +0000

    verifiedFetch

--- assets/ui/packages/services/api.ts
+++ assets/ui/packages/services/api.ts
@@ -1,24 +1,24 @@
-import {verifiedFetch} from '@github-ui/verified-fetch'
+// import {verifiedFetch} from '@github-ui/verified-fetch'
 
-export const request = async (url: string, method: string, formData: FormData) => {
-  try {
-    const response = await verifiedFetch(url, {
-      method,
-      body: formData,
-    })
-    return response.ok
-  } catch (error) {
-    return false
-  }
-}
+// export const request = async (url: string, method: string, formData: FormData) => {
+//   try {
+//     // const response = await verifiedFetch(url, {
+//       method,
+//       body: formData,
+//     })
+//     return response.ok
+//   } catch (error) {
+//     return false
+//   }
+// }
 
-export const deleteRequest = (url: string, feature: string) => {
-  return request(url, 'DELETE', formData(feature))
-}
+// export const deleteRequest = (url: string, feature: string) => {
+//   return request(url, 'DELETE', formData(feature))
+// }
 
-export const createRequest = (url: string, feature: string) => {
-  return request(url, 'POST', formData(feature))
-}
+// export const createRequest = (url: string, feature: string) => {
+//   return request(url, 'POST', formData(feature))
+// }
 
 const formData = (feature: string) => {
   const data = new FormData()
