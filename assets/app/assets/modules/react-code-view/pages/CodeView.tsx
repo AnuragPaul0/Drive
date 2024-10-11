bd075c46dcc310659b1c64e8790fbdbca6a58584
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-10-11 06:30:26 +0000

    s

--- assets/app/assets/modules/react-code-view/pages/CodeView.tsx
+++ assets/app/assets/modules/react-code-view/pages/CodeView.tsx
@@ -29,26 +29,28 @@ export default function CodeView(
     // payload.allShortcutsEnabled
     }>
             {/* <FileTreeControlProvider> */}
-      <div><FileQueryProvider><OpenPanelProvider
-    // payload={payload}
-      openPanelRef={openPanelRef}>
+      <div>
+        {/* <FileQueryProvider><OpenPanelProvider */}
+    {/* // payload={payload} */}
+      {/* openPanelRef={openPanelRef}> */}
       <SplitPageLayout>
         
-    <SplitPageLayout.Header>
-      <Placeholder label="Header" height={100} />
-    </SplitPageLayout.Header>
-    <SplitPageLayout.Pane position="start">
-      <Placeholder label="Pane" height={400} />
-    </SplitPageLayout.Pane>
-    <SplitPageLayout.Content>
-      <Placeholder label="Content" height={600} />
-    </SplitPageLayout.Content>
-    <SplitPageLayout.Footer>
-      <Placeholder label="Footer" height={100} />
-    </SplitPageLayout.Footer>
+        <SplitPageLayout.Header>
+          <Placeholder label="Header" height={100} />
+        </SplitPageLayout.Header>
+        <SplitPageLayout.Pane position="start">
+          <Placeholder label="Pane" height={400} />
+        </SplitPageLayout.Pane>
+        <SplitPageLayout.Content>
+          <Placeholder label="Content" height={600} />
+        </SplitPageLayout.Content>
+        <SplitPageLayout.Footer>
+          <Placeholder label="Footer" height={100} />
+        </SplitPageLayout.Footer>
       </SplitPageLayout>
-      </OpenPanelProvider>
-      </FileQueryProvider></div>
+      {/* </OpenPanelProvider>
+      </FileQueryProvider> */}
+      </div>
             {/* </FileTreeControlProvider> */}
   </AllShortcutsEnabledProvider>
   ) }
