4f2896cb7adc262b100c2b8e104e4044e47c7f1d
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-10-12 17:32:11 +0530

    c

--- assets/ui/packages/react-core/use-current-route-state.ts
+++ assets/ui/packages/react-core/use-current-route-state.ts
@@ -1,10 +1,10 @@
-import {useContext} from 'react'
-import {useLocation} from 'react-router-dom'
-import {RouteStateMapContext} from './route-state-map-context'
+// import {useContext} from 'react'
+// import {useLocation} from 'react-router-dom'
+// import {RouteStateMapContext} from './route-state-map-context'
 
-export function useCurrentRouteState<T>() {
-  const routeStateMap = useContext(RouteStateMapContext)
-  const location = useLocation()
+// export function useCurrentRouteState<T>() {
+//   const routeStateMap = useContext(RouteStateMapContext)
+//   const location = useLocation()
 
-  return routeStateMap[location.key] as T
-}
+//   return routeStateMap[location.key] as T
+// }
