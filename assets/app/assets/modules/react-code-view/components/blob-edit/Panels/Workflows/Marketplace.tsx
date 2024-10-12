dad45a55b1071272b281d27be50c47d0e346b77c
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-10-12 15:35:00 +0530

    js-workflow-editor-sidebar-content

--- assets/app/assets/modules/react-code-view/components/blob-edit/Panels/Workflows/Marketplace.tsx
+++ assets/app/assets/modules/react-code-view/components/blob-edit/Panels/Workflows/Marketplace.tsx
@@ -1,35 +1,35 @@
-import {Box, Spinner} from '@primer/react'
+// import {Box, Spinner} from '@primer/react'
 
-interface WorkflowMarketplaceProps {
-  src?: string
-}
+// interface WorkflowMarketplaceProps {
+//   src?: string
+// }
 
-export function WorkflowMarketplace({src}: WorkflowMarketplaceProps) {
-  import('../../../../../github/editor/actions-sidebar')
+// export function WorkflowMarketplace({src}: WorkflowMarketplaceProps) {
+//   // import('../../../../../github/editor/actions-sidebar')
 
-  return (
-    <Box sx={{px: 3, pt: 3, pb: 0}}>
-      <remote-input
-        param="query"
-        src={src}
-        aria-owns="js-workflow-editor-sidebar-content"
-        class="mb-3 position-relative js-remoteinput"
-      >
-        <input
-          type="text"
-          form="marketplace-search"
-          className="form-control input-block input-contrast"
-          placeholder="Search Marketplace for Actions"
-          aria-label="Search Marketplace for Actions"
-        />
-      </remote-input>
-      <div id="js-workflow-editor-sidebar-content">
-        <include-fragment src={src}>
-          <Spinner sx={{my: 3, mx: 'auto', display: 'block'}} />
-        </include-fragment>
-      </div>
-    </Box>
-  )
-}
+//   return (
+//     <Box sx={{px: 3, pt: 3, pb: 0}}>
+//       <remote-input
+//         param="query"
+//         src={src}
+//         aria-owns="js-workflow-editor-sidebar-content"
+//         class="mb-3 position-relative js-remoteinput"
+//       >
+//         <input
+//           type="text"
+//           form="marketplace-search"
+//           className="form-control input-block input-contrast"
+//           placeholder="Search Marketplace for Actions"
+//           aria-label="Search Marketplace for Actions"
+//         />
+//       </remote-input>
+//       <div id="js-workflow-editor-sidebar-content">
+//         <include-fragment src={src}>
+//           <Spinner sx={{my: 3, mx: 'auto', display: 'block'}} />
+//         </include-fragment>
+//       </div>
+//     </Box>
+//   )
+// }
 
-try{ WorkflowMarketplace.displayName ||= 'WorkflowMarketplace' } catch {}
+// try{ WorkflowMarketplace.displayName ||= 'WorkflowMarketplace' } catch {}
