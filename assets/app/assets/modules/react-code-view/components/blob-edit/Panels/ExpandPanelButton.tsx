4818874750e9bd9fcf7bbd25034642b5a06b4ed7
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-10-12 17:17:06 +0530

    pb

--- assets/app/assets/modules/react-code-view/components/blob-edit/Panels/ExpandPanelButton.tsx
+++ assets/app/assets/modules/react-code-view/components/blob-edit/Panels/ExpandPanelButton.tsx
@@ -1,30 +1,30 @@
-import {ExpandButton} from '@github-ui/code-view-shared/components/ExpandButton'
-import React from 'react'
+// import {ExpandButton} from '@github-ui/code-view-shared/components/ExpandButton'
+// import React from 'react'
 
-interface ExpandPanelButtonProps {
-  expanded?: boolean
-  onToggleExpanded: () => void
-  sx?: Record<string, unknown>
-  ariaControls: string
-}
+// interface ExpandPanelButtonProps {
+//   expanded?: boolean
+//   onToggleExpanded: () => void
+//   sx?: Record<string, unknown>
+//   ariaControls: string
+// }
 
-export const ExpandPanelButton = React.forwardRef(
-  (
-    {expanded, onToggleExpanded, sx, ariaControls}: ExpandPanelButtonProps,
-    ref: React.ForwardedRef<HTMLButtonElement>,
-  ) => (
-    <ExpandButton
-      expanded={expanded}
-      alignment="right"
-      ariaLabel={expanded ? 'Collapse help panel' : 'Expand help panel'}
-      ariaControls={ariaControls}
-      tooltipDirection="nw"
-      testid="help-panel-button"
-      ref={ref}
-      onToggleExpanded={onToggleExpanded}
-      sx={sx}
-    />
-  ),
-)
+// export const ExpandPanelButton = React.forwardRef(
+//   (
+//     {expanded, onToggleExpanded, sx, ariaControls}: ExpandPanelButtonProps,
+//     ref: React.ForwardedRef<HTMLButtonElement>,
+//   ) => (
+//     <ExpandButton
+//       expanded={expanded}
+//       alignment="right"
+//       ariaLabel={expanded ? 'Collapse help panel' : 'Expand help panel'}
+//       ariaControls={ariaControls}
+//       tooltipDirection="nw"
+//       testid="help-panel-button"
+//       ref={ref}
+//       onToggleExpanded={onToggleExpanded}
+//       sx={sx}
+//     />
+//   ),
+// )
 
-ExpandPanelButton.displayName = 'ExpandPanelButton'
+// ExpandPanelButton.displayName = 'ExpandPanelButton'
