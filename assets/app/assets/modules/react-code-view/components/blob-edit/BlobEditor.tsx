603ad7c0a51d40adab75e8a399312b64ebd4a77c
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-10-14 23:42:04 +0530

    l

--- assets/app/assets/modules/react-code-view/components/blob-edit/BlobEditor.tsx
+++ assets/app/assets/modules/react-code-view/components/blob-edit/BlobEditor.tsx
@@ -145,7 +145,7 @@ export default function BlobEditor({
 // mmitDisabledRef.current ||
   return ( <ThemeProvider colorMode={colorMode} dayScheme={dayScheme} nightScheme={nightScheme}
     preventSSRMismatch>
-    <BaseStyles>{/*  , flex: 1
+    {/* <BaseStyles> , flex: 1
         Your ticket will be issued to: <b>{firstName}</b>, justifyContent: 'space-between'
       </p> */}
       {/* <Box sx={{display: 'flex', mb: 3, flexWrap: 'wrap', rowGap: 3,
@@ -192,7 +192,7 @@ export default function BlobEditor({
         </Button> */}
         {/* </Box> */}
       </Box>
-      </BaseStyles>
+      {/* </BaseStyles> */}
   </ThemeProvider>
   )
 }
