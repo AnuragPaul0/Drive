4fca05d57d0bd0acade3bd429524c83bfa06529d
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-10-15 13:42:02 +0530

    l

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,8 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode
   }>) { return <html lang="en">
     <head><script dangerouslySetInnerHTML={{__html:
-`console.log(document, document.querySelector('#left'))
-(()=>{document.querySelector('#left').classList.add(innerWidth < innerHeight ? "mob" : 'desk')})();`}}></script>
+`console.log(document, document.querySelector('#left'));
+(()=>{console.log(document.querySelector('#left'));
+document.querySelector('#left').classList.add(innerWidth < innerHeight ? "mob" : 'desk')})();`}}>
+</script>
 </head>{children}</html> }
