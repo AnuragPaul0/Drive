7cdd39a3f1a5080b31369f40d9ce7e9883706dc9
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-10-15 18:57:55 +0530

    d

--- assets/app/assets/modules/react-code-view/components/blob-edit/BlobEditor.tsx
+++ assets/app/assets/modules/react-code-view/components/blob-edit/BlobEditor.tsx
@@ -15,9 +15,7 @@ export const blobEditSidePanelId = 'blob-edit-side-panel-id'
 export default function BlobEditor() {
   // console.log(1)
   const {colorMode, dayScheme, nightScheme} = useColorModes()
-
   const [firstName, setFirstName] = useState('');
-
   function handleLastNameChange(e: any) { setFirstName(e.target.value) }
 
   useEffect(() => {
@@ -39,24 +37,7 @@ export default function BlobEditor() {
   }, []); // Empty array ensures that effect is only run on mount
 
   // mmitDisabledRef.current ||
-  return ( <ThemeProvider colorMode={colorMode} dayScheme={dayScheme} nightScheme={nightScheme}
-    preventSSRMismatch>
-    {/* <BaseStyles> , flex: 1
-        Your ticket will be issued to: <b>{firstName}</b>, justifyContent: 'space-between'
-      </p> */}
-      {/* <Box sx={{display: 'flex', mb: 3, flexWrap: 'wrap', rowGap: 3,
-          maxWidth: '100%'}}> */}
-         {/* <Box sx={{display: 'flex', alignSelf: 'self-start', alignItems: 'center', pr: 3,
-        maxWidth: '100%'}}>
-        <Box className={size.width < size.height ? "mob" : 'desk'}
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            flex: 1,
-            flexWrap: 'wrap',
-            // maxWidth: showTreeToggle ? 'calc(100% - 75px)' : '100%',
-          }}> */}
-            <Box id='left'>
+  return (<div><Box id='left'>
               <span className="TextInputWrapper__TextInputBaseWrapper-sc-1mqhpbi-0 TextInputWrapper-sc-1mqhpbi-1 lmZlSR igQCti TextInput-wrapper"
                 aria-busy="false">
                 <input value={firstName} onChange={handleLastNameChange}
@@ -64,16 +45,7 @@ export default function BlobEditor() {
                   placeholder={file} className='UnstyledTextInput-sc-14ypya-0 kbCLEG'
                   aria-describedby="file-name-editor-breadcrumb"
               /></span>
-            {/* <TextInput */}
-              {/* // disabled={inputDisabled}
-              // onKeyDown={onFileNameInputKeyPress}fileName
-              // ref={nameInputRef}onFileNameChange */}
-              {/* block={undefined} contrast={undefined} monospace={undefined}
-              width={undefined} maxWidth={undefined} minWidth={undefined} variant={undefined}
-              size={undefined} validationStatus={undefined} as={'input'} disabled={undefined}/> */}
           </Box>
-        {/* </Box>
-      </Box> */}
       <Box id='right'className="tailwind"><BranchName style={{ display: firstName ? 'flex' : 'none' }}
           href={folderPath+firstName}>Open in drive</BranchName>
         {/* <a href={} data-zone="mktng" 
@@ -87,9 +59,7 @@ export default function BlobEditor() {
           >Commit changes...
         </Button> */}
         {/* </Box> */}
-      </Box>
-      {/* </BaseStyles> */}
-  </ThemeProvider>
+      </Box></div>
   )
 }
 
