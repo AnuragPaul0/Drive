994d6457ba83f62bb9d7da0326c6cf0dfa4a7891
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-10-15 15:34:41 +0530

    d

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,38 +11,9 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     {/* <head>
 </head> */}
 <script dangerouslySetInnerHTML={{__html:
-`/**
- * Wait for an element before resolving a promise
- * @param {String} querySelector - Selector of element to wait for
- * @param {Integer} timeout - Milliseconds to wait before timing out, or 0 for no timeout              
- */
-function waitForElement(querySelector, timeout){
-  return new Promise((resolve, reject)=>{
-    var timer = false;
-    if(document.querySelector(querySelector)) return resolve();
-    const observer = new MutationObserver(()=>{
-      if(document.querySelector(querySelector)){
-        observer.disconnect();
-        if(timer !== false) clearTimeout(timer);
-        return resolve();
-      }
-    });
-    observer.observe(document.body, {
-      childList: true, 
-      subtree: true
-    });
-    if(timeout) timer = setTimeout(()=>{
-      observer.disconnect();
-      reject();
-    }, timeout);
-  });
-}
-
-waitForElement("#left", 3000).then(function(){
-    alert("element is loaded.. do stuff");
-}).catch(()=>{
-    alert("element did not load in 3 seconds");
-});console.log(document, document.querySelector('#left'));
+`document.onreadystatechange = () => { console.log(document.readyState)
+document.querySelector('#left').classList.add(innerWidth < innerHeight ? "mob" : 'desk')
+};console.log(document, document.querySelector('#left'));
 (()=>{console.log(document.querySelector('#left'));
 document.querySelector('#left').classList.add(innerWidth < innerHeight ? "mob" : 'desk')})();`}}>
 </script>{children}</html> }
