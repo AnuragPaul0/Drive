24d19d0e25e97b7b1b73d7419954074da2a09136
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-10-15 13:23:30 +0530

    l

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,15 +8,6 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode
   }>) { return <html lang="en">
-    {/* <head><style dangerouslySetInnerHTML={{__html:
-    `span.lmZlSR { background-color: unset; font-size: 14px; line-height: 20px;
-  color: var(--fgColor-default, var(--color-fg-default, #e6edf3)); vertical-align: middle;
-
-  border: 1px solid var(--control-borderColor-rest, var(--borderColor-default,
-    var(--color-border-default, #30363d))); border-radius: 6px; outline: none;
-  
-  box-shadow: var(--shadow-inset, var(--color-primer-shadow-inset, 0 0 transparent));
-  display: inline-flex; -webkit-box-align: stretch; align-items: stretch; min-height: 32px;
-  overflow: hidden; min-width: 100px }`}}
-></style></head> */}
-{children}</html> }
+    <head><script dangerouslySetInnerHTML={{__html:
+`document.querySelector('#left').classList.add(innerWidth < innerHeight ? "mob" : 'desk')`}}></script>
+</head>{children}</html> }
--- assets/app/assets/modules/react-code-view/components/blob-edit/BlobEditor.tsx
+++ assets/app/assets/modules/react-code-view/components/blob-edit/BlobEditor.tsx
@@ -1,15 +1,8 @@
 import {ThemeProvider, BaseStyles} from '@primer/react'
 import useColorModes from './use-color-modes'
-// import type {Repository} from '@github-ui/current-repository'
-import {useCodeViewOptions} from '@github-ui/use-code-view-options'
-import type {WebCommitDialogState} from '@github-ui/web-commit-dialog'
 import {Box, BranchName, Button, TextInput} from '@primer/react'
 import type React from 'react'
 import {useCallback, useEffect, useRef, useState} from 'react'
-// edit
-import {
-  // BlobEditHeader,
-  BlobEditorTab} from './BlobEditHeader'
 
 let fileContentChanged = false, fileName = '', contentChanged = fileContentChanged,
   folderPath = 'https://drive.google.com/viewerng/viewer?embedded=true&url=',
@@ -19,123 +12,12 @@ let fileContentChanged = false, fileName = '', contentChanged = fileContentChang
 
 export const blobEditSidePanelId = 'blob-edit-side-panel-id'
 
-export default function BlobEditor({
-  // collapseTree, editInfo = {enableCommitButton: 1}
-  }
-) {
-  // const {screenSize} = useScreenSize()
-  // const isNarrow = screenSize <= ScreenSize.medium
-  // const [count, setCount] = useState(!0);
-  const [selectedTab, setSelectedTab] = useState<BlobEditorTab>(BlobEditorTab.Edit)
-  const isPreview = selectedTab === BlobEditorTab.Preview
-  const commitChangesRef = useRef<HTMLButtonElement>(null)
-  const [webCommitDialogState, setWebCommitDialogState] = useState<WebCommitDialogState>('closed')
-  // 1
-  //  || editInfo.enableCommitButton 0
-  // This value has to live in a ref because it is used in a callback that is
-  // passed to the code mirror editor. That callback will only be bound once,
-  // so we need a constant reference to the value.
-  // const commitDisabledRef = useRef(false)
-  // folderPath 0 || 0 || 1
-  // commitDisabledRef.current = !(contentChanged || fileNameChanged) || fileName.length === 0
-  // console.log({commitDisabledRef})
-  console.log(1)
-  // const {openPanel, setOpenPanel} = useOpenPanel()
-  // const panelIsOpen = openPanel === 'edit'
+export default function BlobEditor() {
+  // console.log(1)
   const {colorMode, dayScheme, nightScheme} = useColorModes()
-  
-    const {codeCenterOption} = useCodeViewOptions()
-    // , initialFolderPath = foldrPath
-    // const showTreeToggle = !showTree && !repo.isEmpty && !codeCenterOption.enabled
 
-    // const initialFileName = fileNam
-    // , [fileName, setFileName] = useState(initialFileName)
-    // const [folderPath, setFolderPath] = useState(initialFolderPath)
-
-    // const onFileNameInputKeyPress = useCallback(
-    //   (event: React.KeyboardEvent<HTMLInputElement>) => {
-    //     if (
-    //       // eslint-disable-next-line @github-ui/ui-commands/no-manual-shortcut-logic
-    //       event.key === 'Backspace' &&
-    //       folderPath.length > 0 &&
-    //       // nameInputRef.current?.selectionStart === 0 &&
-    //       // nameInputRef.current?.selectionEnd === 0
-    //     ) {
-    //       const pathParts = folderPath
-    //       const newFileName = pathParts
-    //         // [pathParts.length - 2]
-    //         + fileName
-
-    //       // const partLength = pathParts[pathParts.length - 2]!.length
-    //       event.preventDefault()
-          
-    //       // setFileName(newFileName)
-    //       fileName = newFileName
-    //       // console.log({contentChanged, fileNameChanged, fileName})
-    //       // we dont need to call onChange
-    //       // onFileNameInputKeyPress triggers first and then onFileNameChange, nameInputRef
-    //       // onFileNameChange always fires the onChange when needed
-    //     }
-    //   },
-    //   [fileName, folderPath],
-    // )
-
-    const onFileNameChange = useCallback(
-      (event: React.ChangeEvent<HTMLInputElement>) => {
-        const value = event.target.value
-        let newFileName = fileName
-        let newFolderPath = folderPath
-
-          newFolderPath = `${value}`
-        
-        // if (newFolderPath !== initialFolderPath || newFileName !== initialFileName) {
-          // setFolderPath(newFolderPath), fileContentChanged = !0, contentChanged = fileContentChanged,
-            fileName = value, count = !value.length
-            // , commitDisabledRef.current = !()
-            // setCount(!value.length)
-          // setFileName(newFileName)
-          // onChange(newFileName, newFolderPath)
-          console.log({contentChanged, fileNameChanged, fileName
-            // , commitDisabledRef
-          }
-        )
-        // }
-      },
-      [
-        // onChange,, nameInputRef, initialFileName, initialFolderPath
-        fileName, folderPath],
-    )
-
-  // try{ (EditBreadcrumb as any).displayName ||= 'EditBreadcrumb' } catch {}
   const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
 
-  // function EditBreadcrumb({
-  //   showTree =true,
-  //   // treeToggleElement,
-  //   foldrPath = '',
-  //   repo= { isEmpty: !1,
-  //     id: 0,
-  //     name: '',
-  //     ownerLogin: '',
-  //     defaultBranch: '',
-  //     createdAt: '',
-  //     currentUserCanPush: false,
-  //     isFork: false,
-  //     ownerAvatar: '',
-  //     public: false,
-  //     private: false,
-  //     isOrgOwned: false },
-  //   fileNam = '',
-  //   nameInputRef = useRef<HTMLInputElement>(null),
-  //   inputDisabled = !1,
-  // }
-  // ) {
-
-  //   return (
-      
-  //   )
-  // }
   function handleLastNameChange(e: any) { setFirstName(e.target.value) }
   const size = useWindowSize()
 
@@ -161,10 +43,10 @@ export default function BlobEditor({
       
       // Add event listener
       window.addEventListener("resize", handleResize);
-      
+
       // Call handler right away so state gets updated with initial window size
-      handleResize();
-      
+      handleResize()
+
       // Remove event listener on cleanup
       return () => window.removeEventListener("resize", handleResize);
     }, []); // Empty array ensures that effect is only run on mount
@@ -180,7 +62,7 @@ export default function BlobEditor({
           maxWidth: '100%'}}> */}
          {/* <Box sx={{display: 'flex', alignSelf: 'self-start', alignItems: 'center', pr: 3,
         maxWidth: '100%'}}>
-        <BoxinnerWidth < innerHeight
+        <Box
           sx={{
             display: 'flex',
             alignItems: 'center',
