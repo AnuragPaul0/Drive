03b4a71479ccf84157e4c46952ff6b60d2905501
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-10-15 16:15:54 +0530

    od

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,11 +10,29 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   }>) { return <html lang="en">
     {/* <head>;console.log(document, document.querySelector('#left'));
 (()=>{console.log(document.querySelector('#left'));
+document.onreadystatechange = () => { console.log(document.readyState);
+// debugger;
+document.querySelector('#left').classList.add(innerWidth < innerHeight ? "mob" : 'desk')
+}
 document.querySelector('#left').classList.add(innerWidth < innerHeight ? "mob" : 'desk')})();
 </head> */}
 <script dangerouslySetInnerHTML={{__html:
-`document.onreadystatechange = () => { console.log(document.readyState);
-// debugger;
-document.querySelector('#left').classList.add(innerWidth < innerHeight ? "mob" : 'desk')
-}`}}>
+`target = document.querySelector('#left');
+
+// create an observer instance
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+    document.querySelector('#left').classList.add(innerWidth < innerHeight ? "mob" : 'desk')
+        console.log(mutation.type);
+        // Later, you can stop observing
+observer.disconnect();
+    });
+});
+
+// configuration of the observer:
+var config = { attributes: true, childList: true, characterData: true }
+
+// pass in the target node, as well as the observer options
+observer.observe(target, config);
+`}}>
 </script>{children}</html> }
